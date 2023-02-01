997ddd4da338228172fe44f7a83123931519a5b6
lsh9295 <lsh929500@gmail.com>
2023-02-01 13:42:27 +0900

    add: [Admin] complete v1.0.0

--- src/main/java/com/demo/mudang2/src/admin/AdminDao.java
+++ src/main/java/com/demo/mudang2/src/admin/AdminDao.java
@@ -84,8 +84,9 @@ public class AdminDao {
     }
     // 데이터 사용량 확인 조회
     public List<GetDataCheck> getDataCheck() {
-        String getDataCheckQuery = "select busIdx, SUM(if(date(createdAt) = date(NOW()), data, 0)) as 'dayData', SUM(if(date_format(createdAt, '%d') > 12 and date_format(createdAt, '%m') = date_format(NOW(), '%m')\n" +
-                "    or date_format(createdAt, '%d') < 13 and date_format(createdAt, '%m') <= date_format(NOW(), '%m'), data, 'null')) as 'monthData'\n" +
+        String getDataCheckQuery = "select busIdx, SUM(if(date(createdAt) = date(NOW()), data, 0)) as 'dayData',\n" +
+                "       SUM(if((date_format(createdAt, '%m') = date_format(NOW(), '%m') and date_format(NOW(), '%d') > 12 and date_format(createdAt, '%d') > 12)\n" +
+                "    or (date_format(createdAt, '%m') <= date_format(NOW(), '%m') and date_format(createdAt, '%d') > 13), data, 'null')) as 'monthData'\n" +
                 "from data\n" +
                 "group by busIdx";
 
